ebe5b6260c171297ba203576137062417b18c9fd
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2021-12-03 19:44:55 +0500

    Update main.dart
    Adding necessary code for notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-import 'screens/login_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_test_app/screens/register_screen.dart';
+import 'package:firebase_test_app/services/database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-    const MyApp(),
+    MyApp(),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String token = '';
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  Database database = Database();
+
+  @override
+  void initState() {
+    getToken();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      print("onMessage: $message");
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      print("onLaunch: $message");
+      Navigator.pushNamed(context, '/message',
+          arguments: MessageArguments(message, true));
+    });
+
+    super.initState();
+  }
+
+  void getToken() async {
+    token = (await firebaseMessaging.getToken())!;
+    print("Tokennnnnnnn: $token");
+    database.setDeviceToken(token);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +57,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(),
+      home: SignupScreen(),
     );
   }
+}
+
+class MessageArguments {
+  MessageArguments(RemoteMessage message, bool bool);
 }
